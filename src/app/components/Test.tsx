2e0599f3d3b517444808f53003d3321e0a76d6c3
Leprekus <leprekus@yahoo.com>
2023-05-14 22:49:42 -0700

    add auth

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,16 +1,19 @@
 'use client'
 import React from "react";
-import { SignIn, SignInButton, useUser } from "@clerk/nextjs";
+import { SignIn, SignInButton, useClerk, useUser } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/clerk-react";
 
 export default function Test() {
   const user = useUser();
+  
   return (
     <div>
       <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       {
-        user ? 
+        user.isSignedIn ? 
         <>
         <p>Welcome {user.user?.firstName}</p>
+        <SignOutButton/>
         </>:
         <SignInButton />
       }
