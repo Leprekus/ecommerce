6b98cfa69df1e0ab82905af5153390c8f0e558c1
Leprekus <leprekus@yahoo.com>
2023-05-18 21:09:17 -0700

    create and import routers to route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import React, { Suspense } from 'react'
+import { api } from '~/utils/api'
 import Loading from './components/Loading'
 
 export default function Home() {
   const Test = React.lazy(() => import('./components/Test'))
+  const example = api.example.getAll.useQuery()
+  example.data
+  const user = api.example.getAll.useQuery()
+  //when hovering this should match the prisma model its referring to
+  user.data
+
   return (
     <div>
       <Suspense fallback={<Loading/>}>
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,9 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { userRouter } from "./routers/user";
+import { productRouter } from "./routers/product";
+import { categoryRouter } from "./routers/category";
+import { orderRouter } from "./routers/order";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +12,10 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  user: userRouter,
+  product: productRouter,
+  category: categoryRouter,
+  order: orderRouter
 });
 
 // export type definition of API
