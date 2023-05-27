20b638b91e5a693caef9df8a75fa159235e9b799
Leprekus <leprekus@yahoo.com>
2023-05-26 22:41:11 -0700

    create products list

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import CreateProduct from '~/components/Products/CreateProduct'
 
 export default function Dashboard() {
   return (
-    <div>dashboard</div>
+    <div>
+        
+        <CreateProduct/>
+    </div>
   )
 }
